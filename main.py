cf3ad40ec600f5f81a370be27aa3109bddb2fc0d
pupkenberg <anton20514@mail.ru>
2022-02-04 19:00:48 +0400

    map1

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 # -*- coding: utf-8 -*-
 import sys
 
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 from PyQt5.QtWidgets import QApplication
 
 from MainWindow import MainWindow
@@ -10,5 +15,6 @@ from MainWindow import MainWindow
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MainWindow()
+    sys.excepthook = except_hook
     ex.show()
     sys.exit(app.exec())
