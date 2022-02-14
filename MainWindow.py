a073913dc69c80dc7ea71481f2b5942c45f8bc56
Babushkin <anton20514@mail.ru>
2022-02-14 10:20:36 +0400

    №5

--- MainWindow.py
+++ MainWindow.py
@@ -30,14 +30,35 @@ class MainWindow(QMainWindow):
         self.map_types = {'Схема': 'map', 'Спутник': 'sat', 'Гибрид': 'sat,skl'}
 
         self.findButton.clicked.connect(self.find)
+        self.updateCoordsButton.clicked.connect(self.update_coords)
         self.mapTypeBox.currentTextChanged.connect(self.getMapFromCoordinates)
 
         self.long_size = 0.016457
         self.lat_size = 0.00619
 
-    def find(self):
+    def update_coords(self):
         self.coords = self.coordsInput.text()
+        self.point_coords = self.coords
         self.getMapFromCoordinates()
+
+    def find(self):
+        self.search_request = self.searchInput.text()
+        if self.search_request:
+            geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={self.search_request}&format=json"
+            response = requests.get(geocoder_request)
+            if response:
+                json_response = response.json()
+
+                pos = ','.join(json_response["response"]["GeoObjectCollection"]["featureMember"][0][
+                                   "GeoObject"]["Point"][
+                                   "pos"].split())
+                self.coords = pos
+                self.point_coords = pos
+                self.getMapFromCoordinates()
+            else:
+                print("Ошибка выполнения запроса:")
+                print(geocoder_request)
+                print("Http статус:", response.status_code, "(", response.reason, ")")
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_PageUp:
@@ -78,6 +99,25 @@ class MainWindow(QMainWindow):
         self.map_type = self.mapTypeBox.currentText()
         self.map_type = self.map_types[self.map_type]
         self.pixmap.loadFromData(requests.get(
-            f'https://static-maps.yandex.ru/1.x/?ll={self.coords}&spn={self.long_size},{self.lat_size}&l={self.map_type}').content)
+            f'https://static-maps.yandex.ru/1.x/?ll={self.coords}&spn={self.long_size},{self.lat_size}&l={self.map_type}&pt={self.point_coords},pm2blm').content)
         self.map.setPixmap(self.pixmap)
 
+    # def search(self):
+    #     self.search_request = self.searchInput.text()
+    #     if self.search_request:
+    #         geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={self.search_request}&format=json"
+    #         response = requests.get(geocoder_request)
+    #         if response:
+    #             json_response = response.json()
+    #
+    #             pos = ','.join(json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"][
+    #                                "pos"].split())
+    #             self.coords = pos
+    #             self.pixmap.loadFromData(requests.get(
+    #                 f'https://static-maps.yandex.ru/1.x/?ll={self.coords}&spn={self.long_size},{self.lat_size}&l={self.map_type}&pt={self.coords},pm2blm').content)
+    #             print(self.coords)
+    #             self.map.setPixmap(self.pixmap)
+    #         else:
+    #             print("Ошибка выполнения запроса:")
+    #             print(geocoder_request)
+    #             print("Http статус:", response.status_code, "(", response.reason, ")")
